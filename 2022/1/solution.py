dac73dc578838f6ff136dcbbd4c8f24fb480b244
Mahela-DESKTOP <mahela37@gmail.com>
2023-07-11 14:51:06 -0700

    solved day 1 part 2

--- 2022/1/solution.py
+++ 2022/1/solution.py
@@ -1,7 +1,7 @@
 import os, sys
 
 
-def run_solution(input: list[str]):
+def run_solution_first(input: list[str]):
     highest_calories = 0
     calories_per_elf = 0
     for line in input:
@@ -14,9 +14,37 @@ def run_solution(input: list[str]):
     return highest_calories
 
 
+def get_list_entry_default_zero(input: list[any], index: int):
+    try:
+        return input[index]
+    except IndexError:
+        return 0
+
+
+def run_solution_second(input: list[str]):
+    elves = []
+    calories_per_elf = 0
+    for line in input:
+        if line == "\n" or line == None:
+            elves.append(calories_per_elf)
+            calories_per_elf = 0
+        else:
+            calories_per_elf = calories_per_elf + int(line)
+    elves.sort(reverse=True)
+    return (
+        get_list_entry_default_zero(elves, 0)
+        + get_list_entry_default_zero(elves, 1)
+        + get_list_entry_default_zero(elves, 2)
+    )
+
+
 if __name__ == "__main__":
     file = open(os.path.join(sys.path[0], "input.txt"), "r")
     file_contents = file.readlines()
     file.close()
-    solution = run_solution(file_contents)
-    print(solution)
+
+    solution_one = run_solution_first(file_contents)
+    print(f"answer to part one: {solution_one}")
+
+    solution_two = run_solution_second(file_contents)
+    print(f"answer to part two: {solution_two}")
--- 2022/1/tests.py
+++ 2022/1/tests.py
@@ -3,22 +3,68 @@ import solution
 
 
 class TestSolution(unittest.TestCase):
-    def test_1(self):
+    def test_part1_1(self):
         input = ["10", "20", "30", "\n", "2", "4", "6", "\n", "30", "40", "50", None]
         answer = 120
         self.assertEqual(
-            solution.run_solution(input), answer, "Works with multiple elves"
+            solution.run_solution_first(input),
+            answer,
+            "Part 1 - Works with multiple elves",
         )
 
-    def test_2(self):
+    def test_part1_2(self):
         input = ["10", "20", "30", None]
         answer = 60
-        self.assertEqual(solution.run_solution(input), answer, "Works with one elf")
+        self.assertEqual(
+            solution.run_solution_first(input), answer, "Part 1 - Works with one elf"
+        )
 
-    def test_3(self):
+    def test_part1_3(self):
         input = []
         answer = 0
-        self.assertEqual(solution.run_solution(input), answer, "Works with no elves")
+        self.assertEqual(
+            solution.run_solution_first(input), answer, "Part 1 - Works with no elves"
+        )
+
+    def test_part2_1(self):
+        input = [
+            "10",
+            "20",
+            "30",
+            "\n",
+            "2",
+            "4",
+            "6",
+            "\n",
+            "30",
+            "40",
+            "50",
+            "\n",
+            "30",
+            "40",
+            "80",
+            None,
+        ]
+        answer = 330
+        self.assertEqual(
+            solution.run_solution_second(input),
+            answer,
+            "Part 2 - Works with multiple elves",
+        )
+
+    def test_part2_2(self):
+        input = ["10", "20", "30", None]
+        answer = 60
+        self.assertEqual(
+            solution.run_solution_second(input), answer, "Part 2 - Works with one elf"
+        )
+
+    def test_part2_3(self):
+        input = []
+        answer = 0
+        self.assertEqual(
+            solution.run_solution_second(input), answer, "Part 2 - Works with no elves"
+        )
 
 
 if __name__ == "__main__":
